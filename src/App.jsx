526e85f4d0349c6c8400a8f79acbbd7f493c877a
manideep <17101992m@gmail.com>
2022-05-02 02:48:45 +0530

    Hero section completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import discordLogo from "../assets/discord_main_logo.svg";
 import Center_landing from "../assets/center_bg_landing_header.svg";
 import Left_landing from "../assets/left_bg_landing_header.svg";
 import Right_landing from "../assets/right_bg_landing_header.svg";
+import Download_logo from "../assets/download.svg";
 import NavBar from "./components/navbar";
 import HeroSection from "./components/herosection";
 import "./App.css";
@@ -14,6 +15,7 @@ function App() {
         imgLeft={Left_landing}
         imgCenter={Center_landing}
         imgRight={Right_landing}
+        download={Download_logo}
       />
     </div>
   );
--- src/components/herosection.jsx
+++ src/components/herosection.jsx
@@ -4,10 +4,21 @@ import './herosection.css';
 const HeroSection = (props) => {
     return (
         <div className="heroContainer">
-            <img className='center' src={props.imgCenter} alt="" />
+            {/* <img className='center' src={props.imgCenter} alt="" /> */}
             <img className='left' src={props.imgLeft} alt="" />
             <img className='right' src={props.imgRight} alt="" />
+            <span className=" heroHeading">IMAGINE A PLACE...</span>
+            <p className="header2 heroDesc">...where you can belong to a school club, a gaming group, or a worldwide art community.</p>
+            <p className="header2 heroDesc">Where just you and a handful of friends can spend time together. A place that makes it easy</p>
+            <p className="header2 heroDesc">to talk every day and hang out more often.</p>
+            <div className="divButton">
+                <button className='white'>
+                    <img src={props.download} alt="" className="heroDownload" />
+                    Download for Mac</button>
+                <button className='black'>Open Discord in your browser</button>
+            </div>
         </div>
+
     )
 }
 
