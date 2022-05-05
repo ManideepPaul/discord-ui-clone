eb4a01a25848aec318c0ee781d4fbbfa15b31eef
Manideep <17101992m@gmail.com>
2022-05-05 12:52:29 +0530

    Resourse Section structure created

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,24 @@ import React from "react";
 import NavBar from "./components/navbar";
 import HeroSection from "./components/herosection";
 import Invite from "./components/invite";
-import Hanging from "./components/hanging"
+import Hanging from "./components/hanging";
 import Fandom from "./components/fandom";
 import Chillin from "./components/chillin";
+import Resourse from "./components/resourse";
 
 import discordLogo from "../assets/discord_main_logo.svg";
 import Center_landing from "../assets/center_bg_landing_header.svg";
 import Left_landing from "../assets/left_bg_landing_header.svg";
 import Right_landing from "../assets/right_bg_landing_header.svg";
-import Invite_only from "../assets/invite_only_landing.svg"
+import Invite_only from "../assets/invite_only_landing.svg";
 import Hanging_out from "../assets/hanging_out_easy_landing.svg";
-import Fandom_logo from "../assets/fandom_landing.svg"
-import Chillin_img from "../assets/just_chiling_landing.svg"
+import Fandom_logo from "../assets/fandom_landing.svg";
+import Chillin_img from "../assets/just_chiling_landing.svg";
+import USA_flag from "../assets/usa_flag.png";
+import Twitter from "../assets/twitter_icon.svg"
+import Instagram from "../assets/instagram_icon.svg"
+import Facebook from "../assets/facebook_icon.svg"
+import Youtube from "../assets/youtube_icon.svg"
 
 import "./App.css";
 
@@ -28,9 +34,10 @@ function App() {
         imgRight={Right_landing}
       />
       <Invite img={Invite_only} />
-      <Hanging img={Hanging_out}/>
+      <Hanging img={Hanging_out} />
       <Fandom img={Fandom_logo} />
       <Chillin img={Chillin_img} />
+      <Resourse usa={USA_flag} twitter={Twitter} instagram={Instagram} facebook={Facebook} youtube={Youtube} />
     </div>
   );
 }
